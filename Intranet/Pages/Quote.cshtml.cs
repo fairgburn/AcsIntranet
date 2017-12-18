b790d0b2694f1966a9d149a4ddf04eeed38234eb
Brandon Fairburn <bfairburn@acsconveyor.com>
2017-12-18 17:40:11 -0500

    initial commit

---
+++ Intranet/Pages/Quote.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QuotationSystem.Pages
+{
+    public class QuoteModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ Intranet/README.md
@@ -0,0 +1,6 @@
+﻿# ACS Intranet
+
+**12/18/2017 update**
+* Created site layout
+* Nothing really functional yet, just an update to the old look
+* Scales well on phones
